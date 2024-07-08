6c5c441fcaa027f7775ca43b799d1333b8d57986
binch95 <qlscm95@gmail.com>
2024-07-08 16:51:37 +0900

    JDBC연동 후 코드구현

--- src/main/java/org/koreait/JDBC_TEST.java
+++ src/main/java/org/koreait/JDBC_TEST.java
@@ -4,23 +4,31 @@ import java.sql.*;
 
 public class JDBC_TEST {
     static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; //드라이버
-    static String DB_URL = "jdbc:mysql://localhost:3306/"; //접속할 DB 서버
+    static String DB_URL = "jdbc:mysql://localhost:3306/AM_JDBC_2024_07"; //접속할 DB 서버
 
     static String USER_NAME = "root"; //DB에 접속할 사용자 이름
     static String PASSWORD = ""; //사용자의 비밀번호
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String a = "JDBC";
         Connection conn = null;
         Statement state = null;
 //        try { //Reflection 방식
         Class.forName(JDBC_DRIVER);
         conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD); //db 내의 데이터를 저장
         state = conn.createStatement(); //sql 문을 실행하기 위해 conn 연결 정보를 state로 생성
-        conn.setAutoCommit(false);
-        String a = "JDBC";
-        PreparedStatement ps = conn.prepareStatement("create table " + a + " (id int,title TEXT,`body` TEXT)");
-        int i = ps.executeUpdate();
-        conn.commit();
+
+        ResultSet rs = state.executeQuery("select * from " + a);
+        rs.last();
+        int lastid = rs.getRow();
+        System.out.println(lastid);
+        rs.beforeFirst();
+
+        PreparedStatement ps = conn.prepareStatement("insert into " + a + " set title = 'ddd', `body` = 'dnksi';");
+        ps.executeUpdate();
 
 
+        state.close();
+        conn.close();
     }
 }
--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -13,8 +13,18 @@ import java.sql.*;
 
 public class Main {
     static List<Article> articles = new ArrayList();
+    static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; //드라이버
+    static String DB_URL = "jdbc:mysql://localhost:3306/Article"; //접속할 DB 서버
+
+    static String USER_NAME = "root"; //DB에 접속할 사용자 이름
+    static String PASSWORD = ""; //사용자의 비밀번호
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        Statement state = null;
+        Class.forName(JDBC_DRIVER);
+        conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD); //db 내의 데이터를 저장
+        state = conn.createStatement(); //sql 문을 실행하기 위해 conn 연결 정보를 state로 생성
 
         Scanner in = new Scanner(System.in);
         System.out.println("==프로그램 시작==");
@@ -24,6 +34,8 @@ public class Main {
             System.out.print("명령어) ");
             String cmd = in.nextLine().trim();
             String[] answer = cmd.split(" ");
+            String table = answer[0].trim();
+            ResultSet lastId = state.executeQuery("select * from " + table);
             if (cmd.equals("exit")) {
                 System.out.println("==종료==");
                 break;
@@ -37,22 +49,37 @@ public class Main {
                 LocalDateTime now = LocalDateTime.now();
                 String regDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
                 Article article = new Article(articles.size() + 1, title, body);
-                new DBManager(article, answer[1]);
-                System.out.println(DBManager.listId + "번 글이 작성되었습니다");
+                PreparedStatement ps = conn.prepareStatement("insert into " + table + " set title = ?, body = ?");
+                ps.setString(1, title);
+                ps.setString(2, body);
+                ps.executeUpdate();
+                lastId = state.executeQuery("select * from " + table);
+                int id = 0;
+                while (lastId.next()) {
+                    id = lastId.getInt(1);
+                }
+                System.out.println(id + "번 글이 작성되었습니다");
                 articles.add(article);
 
             } else if (cmd.equals("article list")) {
-
-                Article article = null ;
                 System.out.println(" id    /   제목   /   내용   ");
-                new DBManager(article, answer[1]);
-//                if (articles.size() == 0) {
-//                    System.out.println("작성된 글이 없습니다.");
-//                    continue;
-//                }
-//                for (int i = articles.size() - 1; i >= 0; i--) {
-//                    System.out.printf(" %d   /    %s   /     %s   \n", articles.get(i).getId(), articles.get(i).getTitle(), articles.get(i).getTitle());
-//                }
+                while (lastId.next()) {
+                    System.out.printf(" %s   /   %s   /   %s   \n",lastId.getString(1), lastId.getString(2), lastId.getString(3));
+                }
+            }else if (cmd.startsWith("article delete")) {
+                int number = Integer.parseInt(cmd.split(" ")[2]);
+                conn.prepareStatement("delete from " + table + " where id = " + number).execute();
+            }else if (cmd.startsWith("article update")) {
+                int number = Integer.parseInt(cmd.split(" ")[2]);
+                System.out.println("==수정하기==");
+                System.out.print("제목 : ");
+                String title = in.nextLine();
+                System.out.print("내용 : ");
+                String body = in.nextLine();
+                PreparedStatement ps = conn.prepareStatement("update " + table + " set title = ?, body = ? where id = " + number);
+                ps.setString(1, title);
+                ps.setString(2, body);
+                ps.executeUpdate();
             }
 
         }
